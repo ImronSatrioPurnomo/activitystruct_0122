27c5a07267485adfcc20c20276023fef8c89c43c
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-04-01 18:00:07 +0700

    menambahkan kembali code yang sudah kita buat di awal

--- activitystruct_0122/activitystruct_0122.cpp
+++ activitystruct_0122/activitystruct_0122.cpp
@@ -16,7 +16,18 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
+		cout << "Nomor Mahasiswa : ";
+		getline(cin, mhs.nim);
+		cout << "Nama Mahasiswa ";
+		getline(cin, mhs.nama);
 
+		cout << "Alamat Mahasiswa " << endl;
+		cout << "\t Nama Desa : ";
+		cin >> mhs.alamat.desa;
+		cout << "\t Nama Kota";
+		cin >> mhs.alamat.kota;
+		cout << "Umur Mahasiswa ";
+		cin >> mhs.umur;
 	}
 
 	cout << endl;
